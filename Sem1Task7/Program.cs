66b3859a7f78e67daf487c71b8e2a0b818a402ac
Alexey <alexeistarikow@yandex.ru>
2023-01-15 22:24:06 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №7
+// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+
+
+//Считываем данные с консоли
+string? inputNum = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if(inputNum!=null)
+{
+// Парсим введенное число
+    int inputNum1 = int.Parse(inputNum);
+
+int lastDigit = inputNum1%10;
+
+Console.WriteLine(lastDigit);
+}
