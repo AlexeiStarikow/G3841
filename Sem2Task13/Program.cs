b033d9f7c5e1986816ce00b0b4dfa180ddf8ce13
Alexey <alexeistarikow@yandex.ru>
2023-01-21 13:53:10 +0300

    add solution for Task13

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -3,15 +3,39 @@
 
 // Вариант 1
 
-char [] digits = Console.ReadLine().ToCharArray();
-if(digits.Length>2)
+// char [] digits = Console.ReadLine().ToCharArray();
+// if(digits.Length>2)
+// {
+//     Console.WriteLine(digits[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// Вариант 2
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine()??"0");
+// Создаем цикл, если в числе минимум три цифры
+if(number > 99)
 {
-    Console.WriteLine(digits[2]);
+// Находим колличество цифр в числе
+    int length = (int)Math.Log10(number) + 1;
+    int index = 0;
+    int thirdDigit = number;
+// Делаем деление числа на 10 (length - 3) раз, чтобы осталось три цифры в числе
+    while(index < length-3)
+    {
+        thirdDigit = thirdDigit/10;
+        index++;
+    }
+// Находим третью цифру остатком от деления на 10
+    thirdDigit = thirdDigit % 10;
+// Выводим третье число
+    Console.WriteLine(thirdDigit + " является третьей цифрой числа " + number);
 }
 else
 {
-    Console.WriteLine("Третьей цифры нет");
+// Выводим, если в числе меньше трех цифр
+    Console.WriteLine("Число меньше 99, третьей цифры нет");
 }
-
-
- 
