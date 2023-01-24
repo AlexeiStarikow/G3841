f4b23bdecce0de864457e4d2908fe070bb0a84f1
Alexey <alexeistarikow@yandex.ru>
2023-01-24 17:10:27 +0300

    add solution for task 19,21,23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+// Варинт1
+// Считываем данные пользователя
+int n = ReadData("Введите число: ");
+// Проверка на пятизначность
+if (n > 10000&&n<100000)
+{
+    // Проверяем на палиндром
+    if (PalinTest(n) == true)
+    {
+        PrintData("Число является палиндромом.");
+    }
+    else
+    {
+        PrintData("Число не является палиндромом.");
+    }
+}
+else
+{
+    PrintData("Число не пятизначное.");
+}
+Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0 ");
+}
+Выводит результат
+void PrintData(string msg1)
+{
+    Console.WriteLine(msg1);
+}
+// Проверка на палиндром
+bool PalinTest(int n)
+{
+    bool res = false;
+    int d1 = n / 10000;
+    int d2 = (n / 1000) % 10;
+    int d3 = (n % 100) / 10;
+    int d4 = n % 10;
+    res = (d1 == d4) && (d2 == d3) ? true : false;
+
+    return res;
+}
+
+// Вариант 2 не придуман
+// int num=00000;
+// while(num<10000)
+// {
+//     if(PalinTest(num)==true)
+//     {
+//         var palin = new Dictionary<int, int>();
+//         int index = 0;
+//         palin[index] = num;
+//         index++;
+//         num++;
+//     }
+// }
+
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// Вариант 1
+
+// Метод читает данные от пользователя
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+// Выводим результат
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+// Вычисляем расстояние между точками в 3D пространстве
+double CalcLen(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double res = 0;
+    res = Math.Sqrt(Math.Pow(x1 - x2, 2) + (Math.Pow(y1 - y2, 2)) + (Math.Pow(z1 - z2, 2)));
+    return res;
+}
+// // Вводим координаты точек
+// int coordX1 = ReadData("Введите координату X1");
+// int coordX2 = ReadData("Введите координату X2");
+// int coordY1 = ReadData("Введите координату Y1");
+// int coordY2 = ReadData("Введите координату Y2");
+// int coordZ1 = ReadData("Введите координату Z1");
+// int coordZ2 = ReadData("Введите координату Z2");
+// // Вычисляем длину
+// double len = CalcLen(coordX1, coordX2, coordY1, coordY2, coordZ1, coordZ2);
+// // Выдаем результат
+// PrintData("Расстояние между точками: " , len);
+
+// Вариант 2
+// Считываем данные 
+string coord = "A(3,6,5);B(5,4,6)";
+// Создаем массив
+char[] coordArray = coord.ToCharArray();
+// Присваиваем индексы
+for (int i = 0; i < coord.Length; i++)
+{
+    coordArray[i] = coord[i];
+}
+// Вычисляем длину между точками
+double len = CalcLen(coordArray[2], coordArray[11], coordArray[4], coordArray[13], coordArray[6], coordArray[15]);
+// Выдаем результат
+PrintData("Расстояние между точками: ", len);
