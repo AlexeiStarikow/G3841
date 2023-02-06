50cf0a039e36460e9e6913091bbca04149b588ce
Alexey <alexeistarikow@yandex.ru>
2023-02-06 18:17:14 +0300

    add solution for task 41,43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Метод считывает данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+string ReadDataStr(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine();
+}
+
+// Метод проверки массива на кол-во чисел >0
+void CheckNum(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (int.Parse(arr[i]) > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+
+int qNums = ReadData("Введите количество чисел: ");
+string nums = ReadDataStr("Введите числа через запятую");
+string[] digts = nums.Split(new char[] { ',' });
+CheckNum(digts);
+
+
+
+
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем.
+// Пример
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Метод вывода
+void PrintData(string msg1, double msg2, double msg3)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+    Console.WriteLine(msg3);
+}
+
+// Метод считывает данные
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return double.Parse(Console.ReadLine() ?? "0");
+}
+
+double b1 = ReadData("Введите b1 ");
+double k1 = ReadData("Введите k1 ");
+double b2 = ReadData("Введите b2 ");
+double k2 = ReadData("Введите k2 ");
+
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
+
+PrintData("Прямые пересекаются в точке с координатами X,Y", x, y);
+
